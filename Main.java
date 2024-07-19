8044134c3aa9093264d8d9e606bca0fa1be8c0b9
Hikaru Matsuba <h.matsuba.sys23@morijyobi.ac.jp>
2024-07-19 10:48:00 +0900

    勝敗判定をした close #17, close#18

--- Main.java
+++ Main.java
@@ -78,20 +78,13 @@ public class Main {
 
         }
 
-        // 以降にテストコードを追加して検証を行う
-        // 以降にテストコードを追加して検証を行う
-
-        // System.out.println("テスト結果");
-
-        // System.out.println(user.getHand().getHandName());
         // comの手を生成
         com.createRandomHand();
         // 以降にテストコードを追加して検証を行う
+        System.out.println("ランダムなジャンケンの手をCPUにセットする");
 
-System.out.println("ランダムなジャンケンの手をCPUにセットする");
-     
+        System.out.println(com.getHand().getHandName());
 
-System.out.println(com.getHand().getHandName());
     }
 
 }
